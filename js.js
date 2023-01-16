37ab6095507be07ac9f7c0fc32280faa92d77817
TrainGuy53 <trainguy53modelisme@gmail.com>
2023-01-16 22:11:03 +0100

    maj

---
+++ js.js
@@ -0,0 +1,54 @@
+function indexSombre() {
+
+	document.getElementById("tgv").src = "fichiers/tgvSombre.gif";
+	document.getElementById("circuit").src = "fichiers/circuits/circuitSombre.png";
+	document.getElementById("details").src = "fichiers/circuits/detailsSombres.png";
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotivesSombre.png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voituresSombre.png";
+	document.getElementById("sectionDroite").src = "fichiers/icones/wagonsSombre.png";
+
+}
+
+function indexClair() {
+	
+	document.getElementById("tgv").src = "fichiers/tgvClair.gif";
+	document.getElementById("circuit").src = "fichiers/circuits/circuitClair.png";
+	document.getElementById("details").src = "fichiers/circuits/detailsClairs.png";
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotivesClair.png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voituresClair.png";
+	document.getElementById("sectionDroite").src = "fichiers/icones/wagonsClair.png";
+
+}
+
+function locomotivesSombre() {
+
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotivesSombre.png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voituresSombre.png";
+	document.getElementById("sectionDroite").src = "fichiers/icones/wagonsSombre.png";
+	document.getElementById("temps").src = "fichiers/tempsSombre.png";
+
+}
+
+function locomotivesClair() {
+
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotivesClair.png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voituresClair.png";
+	document.getElementById("temps").src = "fichiers/tempsClair.png";
+
+}
+
+function sombre() {
+
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotivesSombre.png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voituresSombre.png";
+	document.getElementById("sectionDroite").src = "fichiers/icones/wagonsSombre.png";
+
+}
+
+function clair() {
+
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotivesClair.png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voituresClair.png";
+	document.getElementById("sectionDroite").src = "fichiers/icones/wagonsClair.png";
+
+}
