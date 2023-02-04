f20a8e782a062af2e566d441f12309b67e8292fb
TrainGuy53 <trainguy53modelisme@gmail.com>
2023-02-04 11:31:59 +0100

    maj

--- js.js
+++ js.js
@@ -1,41 +1,50 @@
-/*
-========================================================================================================================
-=                                                                                                                      =
-=                                             Site créé par Train Guy 53                                               =
-=                                                                                                                      =
-=                                                        js.js                                                         =
-=                                                                                                                      =
-=                                                    Janvier 2023                                                      =
-=                                                                                                                      =
-========================================================================================================================
-*/
+// ================================================================================================================== //
+//                                                                                                                    //
+//                                            Site créé par Train Guy 53                                              //
+//                                                                                                                    //
+//                                                       js.js                                                        //
+//                                                                                                                    //
+//                                                   Janvier 2023                                                     //
+//                                                                                                                    //
+// ================================================================================================================== //
 
-function theme(etat, page) { // change la source des images pour les thèmes
+var theme; // enregistre le thème actuel
 
-	// "etat" vaut "Clair" ou "Sombre" (majuscules importantes)
+function changerDeTheme(page) { // change la source des images pour les thèmes
+
+	// "theme" vaut "Clair" ou "Sombre" (majuscules importantes)
 	// "page" vaut "index", "locomotives", "voitures" ou "wagons"
 
 	"use strict"; // il faut le mettre
 
+	if (theme == 'Sombre') {
+
+		theme = 'Clair';
+
+	} else {
+
+		theme = 'Sombre';
+
+	}
+
 	switch (page) {
 
-		case 'index': // index.html contient des images différentes
+		case 'index': // index.html contient une image particulière
 
-			document.getElementById("tgv").src = "fichiers/tgv" + etat + ".gif";
-			document.getElementById("details").src = "fichiers/circuits/details" + etat + ".png";
+			document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
 			break;
 
 		case 'locomotives': // locomotives.html aussi
 
-			document.getElementById("temps").src = "fichiers/temps" + etat + ".png";
+			document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
 			break;
 
 	}
 
 	// ce qui suit est à executer pour toutes les pages
 
-	document.getElementById("sectionGauche").src = "fichiers/icones/locomotives" + etat + ".png";
-	document.getElementById("sectionMilieu").src = "fichiers/icones/voitures" + etat + ".png";
-	document.getElementById("sectionDroite").src = "fichiers/icones/wagons" + etat + ".png";
+	document.getElementById("sectionGauche").src = "fichiers/icones/locomotives" + theme + ".png";
+	document.getElementById("sectionMilieu").src = "fichiers/icones/voitures" + theme + ".png";
+	document.getElementById("sectionDroite").src = "fichiers/icones/wagons" + theme + ".png";
 
 }
