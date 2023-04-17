58132df25825dd545b31f10cb8fad19188bed942
TrainGuy53 <98115896+TrainGuy53@users.noreply.github.com>
2023-04-17 19:54:52 +0200

    maj

--- js.js
+++ js.js
@@ -4,69 +4,28 @@
 //                                                                                                                   //
 // ================================================================================================================= //
 
-var theme = "Clair";
+var theme = "Clair"; // "theme" vaut "Clair" ou "Sombre" (majuscules importantes)
 
-// function changerDeTheme(pageActuelle) {
-
-// 	// "theme" vaut "Clair" ou "Sombre" (majuscules importantes)
-// 	// "pageActuelle" vaut "index", "locomotives", "voitures" ou "wagons"
-
-// 	"use strict";
-
-// 	if (theme == 'Sombre') {
-
-// 		theme = 'Clair';
-// 		document.getElementById("boutonClair").style.display = "none";
-// 		document.getElementById("boutonSombre").style.display = "block";
-
-// 	} else {
-
-// 		theme = 'Sombre';
-// 		document.getElementById("boutonClair").style.display = "block";
-// 		document.getElementById("boutonSombre").style.display = "none";
-
-// 	}
-
-// 	switch (pageActuelle) {
-
-// 		case 'index':
-
-// 			document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
-// 			break;
-
-// 		case 'locomotives':
-
-// 			document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
-// 			break;
-
-// 	}
-
-// 	document.getElementById("sectionGauche").src = "fichiers/icones/locomotives" + theme + ".png";
-// 	document.getElementById("sectionMilieu").src = "fichiers/icones/voitures" + theme + ".png";
-// 	document.getElementById("sectionDroite").src = "fichiers/icones/wagons" + theme + ".png";
-
-// }
-
-function changerDeTheme() {
+function changerDeTheme(pageActuelle) {
 
 	"use strict";
-
-	console.log(window.parent.document.title);
-
 	if (theme == 'Sombre') {
-
 		theme = 'Clair';
 		document.getElementById("boutonClair").style.display = "none";
 		document.getElementById("boutonSombre").style.display = "block";
-
 	} else {
-
 		theme = 'Sombre';
 		document.getElementById("boutonClair").style.display = "block";
 		document.getElementById("boutonSombre").style.display = "none";
-
 	}
-
+	switch (window.parent.document.title) { // récupère le nom de la page parent
+		case 'Accueil':
+			document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
+			break;
+		case 'Locomotives':
+			document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
+			break;
+	}
 	document.getElementById("sectionGauche").src = "fichiers/icones/locomotives" + theme + ".png";
 	document.getElementById("sectionMilieu").src = "fichiers/icones/voitures" + theme + ".png";
 	document.getElementById("sectionDroite").src = "fichiers/icones/wagons" + theme + ".png";
