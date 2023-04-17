c133ed9fa9f476df7d8e00d0620b8a8fb0473fca
TrainGuy53 <98115896+TrainGuy53@users.noreply.github.com>
2023-04-17 19:49:41 +0200

    maj

--- js.js
+++ js.js
@@ -1,17 +1,57 @@
-// ================================================================================================================== //
-//                                                                                                                    //
-//                                                    Train Guy 53                                                    //
-//                                                                                                                    //
-// ================================================================================================================== //
+// ================================================================================================================= //
+//                                                                                                                   //
+//                                                   Train Guy 53                                                    //
+//                                                                                                                   //
+// ================================================================================================================= //
 
-var theme = "Clair"; // enregistre le thème actuel
+var theme = "Clair";
 
-function changerDeTheme(page) { // change la source des images pour les thèmes, en fonction de la page
+// function changerDeTheme(pageActuelle) {
 
-	// "theme" vaut "Clair" ou "Sombre" (majuscules importantes)
-	// "page" vaut "index", "locomotives", "voitures" ou "wagons"
+// 	// "theme" vaut "Clair" ou "Sombre" (majuscules importantes)
+// 	// "pageActuelle" vaut "index", "locomotives", "voitures" ou "wagons"
+
+// 	"use strict";
+
+// 	if (theme == 'Sombre') {
+
+// 		theme = 'Clair';
+// 		document.getElementById("boutonClair").style.display = "none";
+// 		document.getElementById("boutonSombre").style.display = "block";
+
+// 	} else {
+
+// 		theme = 'Sombre';
+// 		document.getElementById("boutonClair").style.display = "block";
+// 		document.getElementById("boutonSombre").style.display = "none";
+
+// 	}
+
+// 	switch (pageActuelle) {
+
+// 		case 'index':
+
+// 			document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
+// 			break;
+
+// 		case 'locomotives':
+
+// 			document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
+// 			break;
+
+// 	}
+
+// 	document.getElementById("sectionGauche").src = "fichiers/icones/locomotives" + theme + ".png";
+// 	document.getElementById("sectionMilieu").src = "fichiers/icones/voitures" + theme + ".png";
+// 	document.getElementById("sectionDroite").src = "fichiers/icones/wagons" + theme + ".png";
+
+// }
+
+function changerDeTheme() {
 
 	"use strict";
+
+	console.log(window.parent.document.title);
 
 	if (theme == 'Sombre') {
 
@@ -27,29 +67,14 @@ function changerDeTheme(page) { // change la source des images pour les thèmes,
 
 	}
 
-	switch (page) {
-
-		case 'index': // index.html contient une image particulière
-
-			document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
-			break;
-
-		case 'locomotives': // locomotives.html aussi
-
-			document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
-			break;
-
-	}
-
-	// ce qui suit est à executer pour toutes les pages
 	document.getElementById("sectionGauche").src = "fichiers/icones/locomotives" + theme + ".png";
 	document.getElementById("sectionMilieu").src = "fichiers/icones/voitures" + theme + ".png";
 	document.getElementById("sectionDroite").src = "fichiers/icones/wagons" + theme + ".png";
 
 }
 
-// ================================================================================================================== //
-//                                                                                                                    //
-//                                                    Train Guy 53                                                    //
-//                                                                                                                    //
-// ================================================================================================================== //
+// ================================================================================================================= //
+//                                                                                                                   //
+//                                                   Train Guy 53                                                    //
+//                                                                                                                   //
+// ================================================================================================================= //
