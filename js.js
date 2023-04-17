b03a62070ff724205ee36025595596e9475c5a1a
TrainGuy53 <98115896+TrainGuy53@users.noreply.github.com>
2023-04-18 00:33:48 +0200

    maj

--- js.js
+++ js.js
@@ -18,14 +18,14 @@ function changerDeTheme(pageActuelle) {
 		document.getElementById("boutonClair").style.display = "block";
 		document.getElementById("boutonSombre").style.display = "none";
 	}
-	// switch (window.parent.document.title) { // récupère le nom de la page parent
-	// 	case 'Accueil':
-	// 		window.parent.document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
-	// 		break;
-	// 	case 'Locomotives':
-	// 		window.parent.document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
-	// 		break;
-	// }
+	switch (window.parent.document.title) { // récupère le nom de la page parent
+		case 'Accueil':
+			window.parent.document.getElementById("tgv").src = "fichiers/tgv" + theme + ".gif";
+			break;
+		case 'Locomotives':
+			window.parent.document.getElementById("temps").src = "fichiers/temps" + theme + ".png";
+			break;
+	}
 	document.getElementsByClassName("section")[0].src = "fichiers/nav/locomotives" + theme + ".png";
 	document.getElementsByClassName("section")[1].src = "fichiers/nav/voitures" + theme + ".png";
 	document.getElementsByClassName("section")[2].src = "fichiers/nav/wagons" + theme + ".png";
