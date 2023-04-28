e025b265d0c7dde533dfb1988295a4de63ff3163
TrainGuy53 <trainguy53modelisme@gmail.com>
2023-04-28 23:54:06 +0200

    maj

--- js.js
+++ js.js
@@ -4,9 +4,15 @@
 //                                                                                                                   //
 // ================================================================================================================= //
 
+// ================================================================================================================= //
+	// variables
+// ================================================================================================================= //
+
 var themeDeLaPage = "clair";
 const titreDeLaPageParent = window.parent.document.title;
-const url = "https://validator.w3.org/nu/?doc=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2F";
+const urlHtml = "https://validator.w3.org/nu/?doc=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2F";
+const urlCss = "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2Fcss.css";
+const urlJs = "https://js.js";
 const pages = {
 	"Accueil": "index.html",
 	"Locomotives": "locomotives.html",
@@ -15,6 +21,10 @@ const pages = {
 	"Maquettes": "maquettes.html"
 };
 const pause = 3000; // milisecondes
+
+// ================================================================================================================= //
+	// fonctions
+// ================================================================================================================= //
 
 function changerLeThemeDeLaPage() {
 
@@ -30,7 +40,7 @@ function changerLeThemeDeLaPage() {
 	}
 	switch (titreDeLaPageParent) {
 		case "Accueil – Train Guy 53":
-			window.parent.tgv.src = "index/tgv/" + themeDeLaPage + ".gif";
+			window.parent.fond.src = "index/tgv/" + themeDeLaPage + ".png";
 			break;
 		case "Locomotives – Train Guy 53":
 			window.parent.temps.src = "locomotives/temps/" + themeDeLaPage + ".png";
@@ -47,13 +57,13 @@ function validation(langage) {
 	"use strict";
 	switch (langage) {
 		case "html":
-			if (titreDeLaPageParent in pages) {window.parent.location.href = url + pages[titreDeLaPageParent];}
+			if (titreDeLaPageParent in pages) {window.parent.location.href = urlHtml + pages[titreDeLaPageParent];}
 			break;
 		case "css":
-			window.parent.location.href = "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2Fcss.css";
+			window.parent.location.href = urlCss;
 			break;
 		case "js":
-			window.parent.location.href = "";
+			window.parent.location.href = urlJs;
 			break;
 	}
 
