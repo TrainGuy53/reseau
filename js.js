2e18c98a0fa142f4f19453e3ba760d49c0c64222
TrainGuy53 <trainguy53modelisme@gmail.com>
2023-05-14 02:55:38 +0200

    maj

--- js.js
+++ js.js
@@ -8,20 +8,13 @@
 	// variables
 // ================================================================================================================= //
 
-const pause = 3000; // milisecondes
-var themeDeLaPage = "clair";
 const titreDeLaPageParent = window.parent.document.title;
-const pages = {
-	"Accueil – Train Guy 53": "index.html",
-	"Locomotives – Train Guy 53": "locomotives.html",
-	"Voitures – Train Guy 53": "voitures.html",
-	"Wagons – Train Guy 53": "wagons.html",
-	"Maquettes – Train Guy 53": "maquettes.html"
-};
 
 // ================================================================================================================= //
 	// fonctions
 // ================================================================================================================= //
+
+var themeDeLaPage = "clair";
 
 function changerLeThemeDeLaPage() {
 
@@ -52,6 +45,13 @@ function changerLeThemeDeLaPage() {
 const urlHtml = "https://validator.w3.org/nu/?doc=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2F";
 const urlCss = "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ftrainguy53.github.io%2Freseau";
 const urlJs = "https://js.js";
+const pages = {
+	"Accueil – Train Guy 53": "index.html",
+	"Locomotives – Train Guy 53": "locomotives.html",
+	"Voitures – Train Guy 53": "voitures.html",
+	"Wagons – Train Guy 53": "wagons.html",
+	"Maquettes – Train Guy 53": "maquettes.html"
+};
 
 function validation(langage) {
 
@@ -70,10 +70,12 @@ function validation(langage) {
 
 }
 
+const pause = 3000; // milisecondes
+const imgReseau = document.querySelectorAll(".imgReseau");
+const imgWikipediaThumb = document.querySelectorAll(".imgWikipediaThumb");
+
 function basculerImages() {
 
-	const imgReseau = document.querySelectorAll(".imgReseau");
-	const imgWikipediaThumb = document.querySelectorAll(".imgWikipediaThumb");
 	imgReseau.forEach(img => img.style.display = "block");
 	imgWikipediaThumb.forEach(img => img.style.display = "none");
 	setInterval(
@@ -86,21 +88,23 @@ function basculerImages() {
 
 }
 
-function URLWikipedia() {
-	// const debutURLDefaut = "file:///C:/Users/artus/Documents/Train%20Guy%2053/reseau/";
-	const debutURLDefaut = "https://trainguy53.github.io/reseau/";
-	const urlThumb = "https://upload.wikimedia.org/wikipedia/commons/thumb/";
-	const imgThumb = document.getElementsByClassName("imgWikipediaThumb");
-	for (var i = imgThumb.length - 1; i >= 0; i--) {
-		imgThumb[i].src = urlThumb + imgThumb[i].src.split(debutURLDefaut)[1];
-	}
-	const urlWiki = "https://fr.wikipedia.org/wiki/";
-	var wiki = document.getElementsByClassName("wiki");
-		for (var i = wiki.length - 1; i >= 0; i--) {
-		wiki[i].href = urlWiki + wiki[i].href.split(debutURLDefaut)[1];
-	}
+// // const debutURLDefaut = "file:///C:/Users/artus/Documents/Train%20Guy%2053/reseau/";
+// const debutURLDefaut = "https://trainguy53.github.io/reseau/";
+// const imgThumb = document.getElementsByClassName("imgWikipediaThumb");
+// const urlThumb = "https://upload.wikimedia.org/wikipedia/commons/thumb/";
+// const wiki = document.getElementsByClassName("wiki");
+// const urlWiki = "https://fr.wikipedia.org/wiki/";
 
-}
+// function URLWikipedia() {
+
+// 	for (var i = imgThumb.length - 1; i >= 0; i--) {
+// 		imgThumb[i].src = urlThumb + imgThumb[i].src.split(debutURLDefaut)[1];
+// 	}
+// 		for (var i = wiki.length - 1; i >= 0; i--) {
+// 		wiki[i].href = urlWiki + wiki[i].href.split(debutURLDefaut)[1];
+// 	}
+
+// }
 
 // ================================================================================================================= //
 //                                                                                                                   //
