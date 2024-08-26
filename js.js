36fadadd12807b4979dcce98467d41079228e55f
Artus de Chavagnac <trainguy53modelisme@gmail.com>
2024-08-26 15:30:08 +0200

    maj

--- js.js
+++ js.js
@@ -15,38 +15,12 @@ const pages = {
 	"Voitures — Train Guy 53": "voitures.html",
 	"Wagons — Train Guy 53": "wagons.html",
 	"Maquettes — Train Guy 53": "maquettes.html"
+	"Rails — Train Guy 53": "rails.html"
 };
 
 // ================================================================================================================= //
 	// fonctions
 // ================================================================================================================= //
-
-var thème = "clair";
-
-function basculerThème() {
-
-	if (thème === "sombre") {
-		thème = "clair";
-		getElementById("span-clair").style.display = "none";
-		getElementById("span-sombre").style.display = "block";
-	} else {
-		thème = "sombre";
-		getElementById("span-clair").style.display = "block";
-		getElementById("span-sombre").style.display = "none";
-	}
-	switch (titrePageParent) {
-		case pages[0]:
-			window.parent.fond.src = "index/tgv/" + thème + ".png";
-			break;
-		case pages[1]:
-			window.parent.temps.src = "locomotives/temps/" + thème + ".png";
-			break;
-	}
-	for (var i = document.getElementsByClassName("img-fond").length - 1; i >= 0; i--) {
-		document.getElementsByClassName("img-fond")[i].src = "nav/" + thème + "/" + i + ".png";
-	}
-
-}
 
 const urlHtml = "https://validator.w3.org/nu/?doc=https%3A%2F%2Ftrainguy53.github.io%2Freseau%2F";
 const urlCss = "https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Ftrainguy53.github.io%2Freseau";
@@ -68,23 +42,23 @@ function validation(langage) {
 
 }
 
-const pause = 3000; // milisecondes
+// const pause = 3000; // milisecondes
 
-function basculerImages() {
+// function basculerImages() {
 
-	const imgRéseau = document.querySelectorAll(".img-réseau");
-	const imgWikipédia = document.querySelectorAll(".img-wikipédia");
-	imgRéseau.forEach(img => img.style.display = "block");
-	imgWikipédia.forEach(img => img.style.display = "none");
-	setInterval(
-		() => {
-			imgRéseau.forEach(img => img.style.display = img.style.display === "none" ? "block" : "none");
-			imgWikipédia.forEach(img => img.style.display = img.style.display === "none" ? "block" : "none");
-		},
-		pause
-	);
+// 	const imgRéseau = document.querySelectorAll(".img-réseau");
+// 	const imgInternet = document.querySelectorAll(".img-internet");
+// 	imgRéseau.forEach(img => img.style.display = "block");
+// 	imgInternet.forEach(img => img.style.display = "none");
+// 	setInterval(
+// 		() => {
+// 			imgRéseau.forEach(img => img.style.display = img.style.display === "none" ? "block" : "none");
+// 			imgInternet.forEach(img => img.style.display = img.style.display === "none" ? "block" : "none");
+// 		},
+// 		pause
+// 	);
 
-}
+// }
 
 // ================================================================================================================= //
 //                                                                                                                   //
